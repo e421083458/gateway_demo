16a43deab81b46b958b640713979882792db96f0
niuyufu <niuyufu@didichuxing.com>
2020-04-28 18:46:29 +0800

    增加思维导图

--- onekeysynccode.sh
+++ onekeysynccode.sh
@@ -6,7 +6,11 @@ cp -rf * ../tmp_dir
 cd ../tmp_dir
 rm -rf .git
 mv README.md README_bak.md
-cat README_bak.md | awk '{gsub(/github.com\/e421083458/,"git.imooc.com/coding-436"); print $0}' > README.md
+cat README_bak.md | awk '{gsub(/github.com\/e421083458\/gateway_demo\/tree/,"git.imooc.com/coding-436/gateway_demo/src"); print $0}' > README_bak2.md
+cat README_bak2.md
+cat README_bak2.md | awk '{gsub(/github.com\/e421083458/,"git.imooc.com/coding-436"); print $0}' > README.md
+cat README.md
+rm README_bak2.md
 rm README_bak.md
 cp -rf * /Users/niuyufu/imooc/gateway_demo
 cd ../
